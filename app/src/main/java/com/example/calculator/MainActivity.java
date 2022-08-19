bab1d27e4f805072581b7246b19be4b11bebbbf8
patel1597 <90780076+patel1597@users.noreply.github.com>
2022-08-19 08:20:05 -0500

    MadeTheMainPage

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void switchScreens(View view) {
+        EditText editText = findViewById(R.id.enteredName);
+        String name = editText.getText().toString();
+        Intent intent = new Intent(this, Calc.class);
+        intent.putExtra("NAME", name);
+        startActivity(intent);
+    }
+
+}
