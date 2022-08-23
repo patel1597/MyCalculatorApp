1e833c2c3103d033ccd8a6dc4d31acee1711eeda
patel1597 <prahil1625@gmail.com>
2022-08-22 22:08:02 -0500

    completedBasicVersion

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -9,19 +9,72 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    private EditText Value1;
-    private EditText Value2;
+    private EditText value1;
+    private EditText value2;
     private Button add;
     private Button subtract;
     private Button multiply;
     private Button divide;
     private EditText totalValue;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        value1 = (EditText) findViewById(R.id.value1);
+        value2 = (EditText) findViewById(R.id.value2);
+        add = (Button) findViewById(R.id.add);
+        subtract = (Button) findViewById(R.id.subtract);
+        multiply = (Button) findViewById(R.id.multiply);
+        divide = (Button) findViewById(R.id.divide);
+        totalValue = (EditText) findViewById(R.id.totalResult);
+    }
 
+    public void add(View view){
+        String v1 = value1.getText().toString();
+        String v2 = value2.getText().toString();
+
+        double n1 = Double.parseDouble(v1);
+        double n2 = Double.parseDouble(v2);
+
+        double total = n1 + n2;
+
+        totalValue.setText(Double.toString(total));
+    }
+
+    public void subtract(View view){
+        String v1 = value1.getText().toString();
+        String v2 = value2.getText().toString();
+
+        double n1 = Double.parseDouble(v1);
+        double n2 = Double.parseDouble(v2);
+
+        double total = n1 - n2;
+
+        totalValue.setText(Double.toString(total));
+    }
+
+    public void multiply(View view){
+        String v1 = value1.getText().toString();
+        String v2 = value2.getText().toString();
+
+        double n1 = Double.parseDouble(v1);
+        double n2 = Double.parseDouble(v2);
+
+        double total = n1 * n2;
+
+        totalValue.setText(Double.toString(total));
+    }
+
+    public void divide(View view){
+        String v1 = value1.getText().toString();
+        String v2 = value2.getText().toString();
+
+        double n1 = Double.parseDouble(v1);
+        double n2 = Double.parseDouble(v2);
+
+        double total = n1 / n2;
+
+        totalValue.setText(Double.toString(total));
+    }
 }
