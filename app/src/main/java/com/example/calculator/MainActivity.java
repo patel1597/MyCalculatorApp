273ee6600e835f8d075de0343921c1d13b3d52a4
patel1597 <90780076+patel1597@users.noreply.github.com>
2022-08-24 08:20:13 -0500

    made square button

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,29 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.text.DecimalFormat;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText value1;
     private EditText value2;
+    private EditText value3;
     private Button add;
     private Button subtract;
     private Button multiply;
     private Button divide;
+    private Button mod;
+    private Button square;
     private EditText totalValue;
+    private EditText valueForSquareAndSquareRoot;
+    // I used this website to find out how to round to three decimal places: https://mkyong.com/java/how-to-round-double-float-value-to-2-decimal-points-in-java/
+    private final DecimalFormat threeDecimalPlaces = new DecimalFormat("0.000");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         value1 = (EditText) findViewById(R.id.value1);
         value2 = (EditText) findViewById(R.id.value2);
+        value3 = (EditText) findViewById(R.id.value3);
         add = (Button) findViewById(R.id.add);
         subtract = (Button) findViewById(R.id.subtract);
         multiply = (Button) findViewById(R.id.multiply);
         divide = (Button) findViewById(R.id.divide);
+        mod = (Button) findViewById(R.id.mod);
+        square = (Button) findViewById(R.id.square);
         totalValue = (EditText) findViewById(R.id.totalResult);
     }
 
@@ -40,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
         double total = n1 + n2;
 
-        totalValue.setText("" + total);
+        totalValue.setText("" + threeDecimalPlaces.format(total));
     }
 
     public void findDifference(View view){
@@ -52,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
         double total = n1 - n2;
 
-        totalValue.setText("" + total);
+        totalValue.setText("" + threeDecimalPlaces.format(total));
     }
 
     public void findProduct(View view){
@@ -64,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
         double total = n1 * n2;
 
-        totalValue.setText("" + total);
+        totalValue.setText("" + threeDecimalPlaces.format(total));
     }
 
     public void findQuotient(View view){
@@ -76,6 +89,29 @@ public class MainActivity extends AppCompatActivity {
 
         double total = n1 / n2;
 
-        totalValue.setText("" + total);
+        totalValue.setText("" + threeDecimalPlaces.format(total));
     }
+
+    public void findModulus(View view){
+        String v1 = value1.getText().toString();
+        String v2 = value2.getText().toString();
+
+        double n1 = Double.parseDouble(v1);
+        double n2 = Double.parseDouble(v2);
+
+        double total = n1 % n2;
+
+        totalValue.setText("" + threeDecimalPlaces.format(total));
+    }
+
+    public void square(View view){
+        String v3 = value3.getText().toString();
+
+        double n1 = Double.parseDouble(v3);
+
+        double total = Math.pow(n1, 2);
+
+        totalValue.setText("" + threeDecimalPlaces.format(total));
+    }
+
 }
