ee0f075f6edd51335aec18aab7e3f13202bf6d5b
patel1597 <prahil1625@gmail.com>
2022-08-25 19:17:05 -0500

    finalCode
    made the app unbreakable

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,16 +3,13 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.text.DecimalFormat;
-
 
 public class MainActivity extends AppCompatActivity {
     private EditText value1;
@@ -24,14 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private Button divide;
     private Button mod;
     private Button square;
+    private Button clear;
     private EditText totalValue;
-    private EditText valueForSquareAndSquareRoot;
     // I used this website to find out how to round to three decimal places: https://mkyong.com/java/how-to-round-double-float-value-to-2-decimal-points-in-java/
     private final DecimalFormat threeDecimalPlaces = new DecimalFormat("0.000");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         value1 = (EditText) findViewById(R.id.value1);
@@ -43,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         divide = (Button) findViewById(R.id.divide);
         mod = (Button) findViewById(R.id.mod);
         square = (Button) findViewById(R.id.square);
+        clear = (Button) findViewById(R.id.clear);
         totalValue = (EditText) findViewById(R.id.totalResult);
     }
 
@@ -50,76 +47,88 @@ public class MainActivity extends AppCompatActivity {
         String v1 = value1.getText().toString();
         String v2 = value2.getText().toString();
 
-        double n1 = Double.parseDouble(v1);
-        double n2 = Double.parseDouble(v2);
+        if (!v1.equals("") || !v2.equals("")) {
+            double n1 = Double.parseDouble(v1);
+            double n2 = Double.parseDouble(v2);
 
-        double total = n1 + n2;
+            double total = n1 + n2;
 
-        totalValue.setText("" + threeDecimalPlaces.format(total));
+            totalValue.setText("" + threeDecimalPlaces.format(total));
+        }
     }
 
     public void findDifference(View view){
-
         String v1 = value1.getText().toString();
         String v2 = value2.getText().toString();
 
-        double n1 = Double.parseDouble(v1);
-        double n2 = Double.parseDouble(v2);
+        if (!v1.equals("") || !v2.equals("")) {
+            double n1 = Double.parseDouble(v1);
+            double n2 = Double.parseDouble(v2);
 
-        double total = n1 - n2;
+            double total = n1 - n2;
 
-        totalValue.setText("" + threeDecimalPlaces.format(total));
+            totalValue.setText("" + threeDecimalPlaces.format(total));
+        }
     }
 
     public void findProduct(View view){
-
         String v1 = value1.getText().toString();
         String v2 = value2.getText().toString();
 
-        double n1 = Double.parseDouble(v1);
-        double n2 = Double.parseDouble(v2);
+        if (!v1.equals("") || !v2.equals("")) {
+            double n1 = Double.parseDouble(v1);
+            double n2 = Double.parseDouble(v2);
 
-        double total = n1 * n2;
+            double total = n1 * n2;
 
-        totalValue.setText("" + threeDecimalPlaces.format(total));
+            totalValue.setText("" + threeDecimalPlaces.format(total));
+        }
     }
 
     public void findQuotient(View view){
-
         String v1 = value1.getText().toString();
         String v2 = value2.getText().toString();
 
-        double n1 = Double.parseDouble(v1);
-        double n2 = Double.parseDouble(v2);
+        if (!v1.equals("") || !v2.equals("")) {
+            double n1 = Double.parseDouble(v1);
+            double n2 = Double.parseDouble(v2);
 
-        double total = n1 / n2;
+            double total = n1 / n2;
 
-        totalValue.setText("" + threeDecimalPlaces.format(total));
+            totalValue.setText("" + threeDecimalPlaces.format(total));
+        }
     }
 
     public void findModulus(View view){
-
         String v1 = value1.getText().toString();
         String v2 = value2.getText().toString();
 
-        double n1 = Double.parseDouble(v1);
-        double n2 = Double.parseDouble(v2);
+        if (!v1.equals("") || !v2.equals("")) {
+            double n1 = Double.parseDouble(v1);
+            double n2 = Double.parseDouble(v2);
 
-        double total = n1 % n2;
+            double total = n1 % n2;
 
-        totalValue.setText("" + threeDecimalPlaces.format(total));
+            totalValue.setText("" + threeDecimalPlaces.format(total));
+        }
     }
 
-    public void square(View view){
+    public void square(View view) {
         String v3 = value3.getText().toString();
 
-        double n1 = Double.parseDouble(v3);
+        if (!v3.equals("")) {
+            double n1 = Double.parseDouble(v3);
 
-        double total = Math.pow(n1, 2);
+            double total = Math.pow(n1, 2);
 
-        totalValue.setText("" + threeDecimalPlaces.format(total));
+            totalValue.setText("" + threeDecimalPlaces.format(total));
+        }
     }
 
-
-
+    public void clear(View view){
+        value1.setText("");
+        value2.setText("");
+        value3.setText("");
+        totalValue.setText("0.000");
+    }
 }
